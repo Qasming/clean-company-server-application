90bcf96907b117845feefe2270a0ebf912d4d10d
Maxim Boydenko <40786356+Qasming@users.noreply.github.com>
2022-04-04 19:15:41 +0700

    Update config.py
    Changed default settings

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-JWT_SECRET_KEY = "SUPERPUPERSECKERJWTKEYFORMYAPPLICATION"
+JWT_SECRET_KEY = ""
 
 DATABASE = os.path.join(ROOT_DIR, 'clean_company_db.db')
 
 DEBUG_MODE = True
 
-SMS_RU_ID = "0C6A5988-002F-A9B2-9EC2-13B404A3DE49"
+SMS_RU_ID = ""
